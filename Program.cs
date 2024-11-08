bf1410357df3471ba9f7a87e72e24c287fb4e016
marco-soria <marco.soriaa7@gmail.com>
2024-11-08 10:24:20 -0500

    added toast

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using blazorappdemov1;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
+
+builder.Services.AddBlazoredToast();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl!) });
 
 builder.Services.AddScoped<IProductService, ProductService>();
